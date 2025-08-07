4627c41a982daece45f60c0277bd050004a11399
bhushan354 <deshmukhumesh354@gmail.com>
2025-08-07 17:31:49 +0530

    basic frontend setup done router axios

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogOut() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={
+            <ProtectedRoute><Home /></ProtectedRoute>
+          }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterAndLogOut />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <p>Please fill out the form to create an account.</p>
+    </div>
+  );
+}  
+
+export default Register;
